b417785e6aa25da8e7e84052d103f14fece19719
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-10-30 11:28:46 -0600

    Lenguage Btn and Status Circle BoxShadow

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,25 @@
-let url = "https://rickandmortyapi.com/api/character";
+const url = "https://rickandmortyapi.com/api/character";
 const dataDiv = document.getElementById("Datadiv");
 const LoaderPortal = document.querySelector(".loader-div");
+const LenguageBtn = document.getElementById("changeLenguage_btn");
+
+let LenguagePrefference = JSON.parse(localStorage.getItem("Lenguage"));
+
+if (LenguagePrefference === null) {
+  const Lenguage = {
+    LenguagePreff: true,
+    TextBtn: "ENG",
+  };
+
+  let Lenguage_ENG = Lenguage;
+  localStorage.setItem("Lenguage", JSON.stringify(Lenguage_ENG));
+
+  LenguagePrefference = JSON.parse(localStorage.getItem("Lenguage"));
+} else {
+  //console.log("LENGUAGEPREFFERENCE VARIABLE HAVE NOW A VALUE :)");
+}
+
+LenguageBtn.innerText = JSON.parse(localStorage.getItem("Lenguage")).TextBtn;
 
 const FetchData = async () => {
   await fetch(url)
@@ -9,20 +28,101 @@ const FetchData = async () => {
     .catch((err) => console.error(err));
 };
 
+LenguageBtn.addEventListener("click", (e) => {
+  //e.preventDefault();
+  let value = JSON.parse(localStorage.getItem("Lenguage")).LenguagePreff;
+
+  if (!value) {
+    const Lenguage = {
+      LenguagePreff: true,
+      TextBtn: "ENG",
+    };
+
+    let Lenguage_ENG = Lenguage;
+    localStorage.setItem("Lenguage", JSON.stringify(Lenguage_ENG));
+
+    let NEWTEXTBTN = JSON.parse(localStorage.getItem("Lenguage")).TextBtn;
+    LenguageBtn.innerText = NEWTEXTBTN;
+
+    return location.href = `index.html`;
+  } else {
+    const Lenguage = {
+      LenguagePreff: false,
+      TextBtn: "ESP",
+    };
+
+    let Lenguage_ESP = Lenguage;
+    localStorage.setItem("Lenguage", JSON.stringify(Lenguage_ESP));
+
+    let NEWTEXTBTN = JSON.parse(localStorage.getItem("Lenguage")).TextBtn;
+    LenguageBtn.innerText = NEWTEXTBTN;
+
+    return location.href = `index.html`;
+  }
+});
+
 const MostrarData = (ApiData) => {
+  const Translations_Status = {
+    Alive: "Con vida",
+    Dead: "Muerto",
+    unknown: "Desaparecido",
+  };
+
+  const Translations_Gender = {
+
+  }
+
   ApiData.forEach((character) => {
+    console.log(character)
     const CharacterName = document.createElement("h1");
     CharacterName.textContent = character.name;
 
-    const CharacterStatus = document.createElement("p");
-    CharacterStatus.textContent = character.status;
+    const StatusDiv = document.createElement("div");
+    StatusDiv.classList.add("CharStatusDiv");
 
-    const CharacterBtn = document.createElement("button");
-    CharacterBtn.textContent = "Ver mas..";
-    CharacterBtn.setAttribute("id", character.id);
+    const CharacterStatus = document.createElement("p");
+    if (!JSON.parse(localStorage.getItem("Lenguage")).LenguagePreff === false) {
+      switch (character.status) {
+        case "Alive":
+          CharacterStatus.innerText = "Estado: " + Translations_Status.Alive;
+          break;
+        case "Dead":
+          CharacterStatus.innerText = "Estado: " + Translations_Status.Dead;
+          break;
+        case "unknown":
+          CharacterStatus.innerText = "Estado: " + Translations_Status.unknown;
+          break;
+      }
+    } else {
+      CharacterStatus.innerText = "Status: " + character.status;
+    }
 
     const StatusColor = document.createElement("div");
-    StatusColor.setAttribute("class", "CharStatus");
+    StatusDiv.classList.add("CharStatus");
+
+    const CharacterBtn = document.createElement("button");
+    if (!JSON.parse(localStorage.getItem("Lenguage")).LenguagePreff === false) {
+      switch (character.status) {
+        case "Alive":
+          CharacterStatus.innerText = "Estado: " + Translations_Status.Alive;
+          break;
+        case "Dead":
+          CharacterStatus.innerText = "Estado: " + Translations_Status.Dead;
+          break;
+        case "unknown":
+          CharacterStatus.innerText = "Estado: " + Translations_Status.unknown;
+          break;
+      }
+    } else {
+      CharacterStatus.innerText = "Status: " + character.status;
+    }
+
+    if (!JSON.parse(localStorage.getItem("Lenguage")).LenguagePreff === false) {
+      CharacterBtn.textContent = "Ver más";
+    } else {
+      CharacterBtn.textContent = "Show more";
+    }
+    CharacterBtn.setAttribute("id", character.id);
 
     switch (character.status) {
       case "Alive":
@@ -38,20 +138,17 @@ const MostrarData = (ApiData) => {
 
     dataDiv.appendChild(CharacterName);
     dataDiv.appendChild(CharacterStatus);
-    CharacterStatus.appendChild(StatusColor);
+    StatusDiv.appendChild(CharacterStatus);
+    StatusDiv.appendChild(StatusColor);
+    dataDiv.appendChild(StatusDiv);
     dataDiv.appendChild(CharacterBtn);
   });
 };
-
-FetchData();
 
 dataDiv.addEventListener("click", async (e) => {
   let target = e.target.id;
 
   if (target != "" && target != "Datadiv") {
-    console.log(e.target.id);
-    console.log(typeof target);
-
     LoaderPortal.classList.remove("loading_off");
 
     await fetch(`https://rickandmortyapi.com/api/character/${target}`)
@@ -69,3 +166,5 @@ dataDiv.addEventListener("click", async (e) => {
     }, 1000);
   }
 });
+
+FetchData();
