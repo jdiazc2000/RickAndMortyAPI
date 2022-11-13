e3e690661ed86a57d1e588927d321d54e3239696
jdiazc2000 <julio_diaz_cortegana@hotmail.com>
2022-11-13 00:00:57 -0500

    Styled character page

--- scripts/character.js
+++ scripts/character.js
@@ -10,16 +10,27 @@ setTimeout(() => {
 
 if(CharacterSelected != null){
     MainDiv.innerHTML += `
-    <img src="${CharacterSelected.image}">
+    <div id="CharacterData">
+    <img src="${CharacterSelected.image}" title="CharImg">
+    <div class="CharacterInfo">
     <h1>${CharacterSelected.name}</h1>
     <p>${'Gender: ' + CharacterSelected.gender}</p>
     <p>${'Status: ' + CharacterSelected.status}</p>
-    <o>${'Origin: ' + CharacterSelected.origin.name}</o>
+    <p>${'Origin: ' + CharacterSelected.origin.name}</p>
+    <p>${'Specie: ' + CharacterSelected.species}</p>
+    <p>${'Location: ' + CharacterSelected.location.name}</p>
+    <button id="returnBtn" title="returnHomePage"><i class="fa-solid fa-person-walking-arrow-loop-left"></i></button>
+    </div>
+    </div>
 `
 }else{
     location.href =`index.html`;
 }
 
+
+document.getElementById('returnBtn').addEventListener('click', () => {
+    location.href =`index.html`;
+})
 
 
 
